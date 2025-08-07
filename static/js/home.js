ee98783a3801afe64a9a58ba5a81a4e422951552
SahilSharma <sahilsharma3043@gmail.com>
2025-08-08 00:51:16 +0530

    git push

--- static/js/home.js
+++ static/js/home.js
@@ -10,13 +10,6 @@ beta.addEventListener("click", () => {
     window.location.href = "/resources";
 });
 
-<<<<<<< HEAD
-
-let gamma = document.querySelector(".Syllabus");
-
-gamma.addEventListener("click", () => {
-    window.location.href = "/Syllabus";
-=======
 let gamma = document.querySelector(".syllabus");
 
 gamma.addEventListener("click", () => {
@@ -28,5 +21,4 @@ let delta = document.querySelector(".links");
 
 delta.addEventListener("click", () => {
     window.location.href = "/links";
->>>>>>> main
 });
